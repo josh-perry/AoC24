3142ebee912df8bbd6377709a0801f26cf8b7d1d
Josh Perry <josh-perry@users.noreply.github.com>
2024-12-02 22:07:10 +0000

    Day 2 - part 2

--- AoC24.Test/Day2.cs
+++ AoC24.Test/Day2.cs
@@ -23,4 +23,27 @@ public class Day2
         // Assert
         Assert.Equal("2", result);
     }
+    
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part2()
+    {
+        // Arrange
+        var input = """
+                    7 6 4 2 1
+                    1 2 7 8 9
+                    9 7 6 2 1
+                    1 3 2 4 5
+                    8 6 4 4 1
+                    1 3 6 7 9
+                    """;
+
+        
+        var day = new Days.Day2();
+
+        // Act
+        var result = day.Part2(input);
+
+        // Assert
+        Assert.Equal("4", result);
+    }
 }
--- AoC24/Days/Day2.cs
+++ AoC24/Days/Day2.cs
@@ -9,53 +9,72 @@ public class Day2 : IDay
         Decreasing,
         Increasing
     }
+    
+    private List<List<int>> ParseInput(string input)
+    {
+        var reports = input.Split("\n");
+        var parsedReports = new List<List<int>>();
+
+        foreach (var report in reports)
+        {
+            var numbers = report.Split(" ").Select(int.Parse).ToList();
+            parsedReports.Add(numbers);
+        }
+
+        return parsedReports;
+    }
+
+    private bool IsReportSafe(List<int> report)
+    {
+        int? lastNumber = null;
+        Direction? direction = null;
+
+        for (var index = 0; index < report.Count; index++)
+        {
+            var number = report[index];
+            
+            if (lastNumber is null)
+            {
+                lastNumber = number;
+                continue;
+            }
+
+            var difference = lastNumber.Value - number;
+            lastNumber = number;
+
+            var newDirection = difference switch
+            {
+                < 0 => Direction.Increasing,
+                > 0 => Direction.Decreasing,
+                _ => direction
+            };
+
+            direction ??= newDirection;
+
+            if (direction != newDirection)
+            {
+                return false;
+            }
+
+            if (Math.Abs(difference) is >= 1 and <= 3)
+            {
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
 
     public string Part1(string input)
     {
-        var reports = input.Split("\n");
+        var reports = ParseInput(input);
         var safeReports = 0;
 
         foreach (var report in reports)
         {
-            var numbers = report.Split(" ").Select(int.Parse);
-            int? lastNumber = null;
-            var safe = true;
-            Direction? direction = null;
-
-            foreach (var number in numbers)
-            {
-                if (lastNumber is null)
-                {
-                    lastNumber = number;
-                    continue;
-                }
-                
-                var difference = lastNumber.Value - number;
-                lastNumber = number;
-                
-                var newDirection = difference switch
-                {
-                    < 0 => Direction.Increasing,
-                    > 0 => Direction.Decreasing,
-                    _ => direction
-                };
-                
-                direction ??= newDirection;
-                
-                if (direction != newDirection)
-                {
-                    safe = false;
-                    break;
-                }
-
-                if (Math.Abs(difference) is >= 1 and <= 3)
-                {
-                    continue;
-                }
-                
-                safe = false;
-            }
-            
+            var safe = IsReportSafe(report);
             safeReports += safe ? 1 : 0;
         }
 
@@ -64,6 +83,31 @@ public class Day2 : IDay
 
     public string Part2(string input)
     {
-        return string.Empty;
+        var reports = ParseInput(input);
+        var safeReports = 0;
+
+        foreach (var report in reports)
+        {
+            for (var retry = -1; retry < report.Count; retry++)
+            {
+                var adjustedReport = report;
+                
+                if (retry != -1)
+                {
+                    adjustedReport = new List<int>(report);
+                    adjustedReport.RemoveAt(retry);
+                }
+
+                if (!IsReportSafe(adjustedReport))
+                {
+                    continue;
+                }
+                
+                safeReports += 1;
+                break;
+            }
+        }
+
+        return safeReports.ToString();
     }
 }

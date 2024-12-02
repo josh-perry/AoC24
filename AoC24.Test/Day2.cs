84d9ede746c1104e5e78a1edb147757a582053d9
Josh Perry <josh-perry@users.noreply.github.com>
2024-12-02 21:41:41 +0000

    Day 2 - part 1

---
+++ AoC24.Test/Day2.cs
@@ -0,0 +1,26 @@
+namespace AoC24.Test;
+
+public class Day2
+{
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part1()
+    {
+        // Arrange
+        var input = """
+                    7 6 4 2 1
+                    1 2 7 8 9
+                    9 7 6 2 1
+                    1 3 2 4 5
+                    8 6 4 4 1
+                    1 3 6 7 9
+                    """;
+        
+        var day = new Days.Day2();
+
+        // Act
+        var result = day.Part1(input);
+
+        // Assert
+        Assert.Equal("2", result);
+    }
+}
---
+++ AoC24/Days/Day2.cs
@@ -0,0 +1,69 @@
+﻿namespace AoC24.Days;
+
+public class Day2 : IDay
+{
+    public int Day => 2;
+
+    private enum Direction
+    {
+        Decreasing,
+        Increasing
+    }
+
+    public string Part1(string input)
+    {
+        var reports = input.Split("\n");
+        var safeReports = 0;
+
+        foreach (var report in reports)
+        {
+            var numbers = report.Split(" ").Select(int.Parse);
+            int? lastNumber = null;
+            var safe = true;
+            Direction? direction = null;
+
+            foreach (var number in numbers)
+            {
+                if (lastNumber is null)
+                {
+                    lastNumber = number;
+                    continue;
+                }
+                
+                var difference = lastNumber.Value - number;
+                lastNumber = number;
+                
+                var newDirection = difference switch
+                {
+                    < 0 => Direction.Increasing,
+                    > 0 => Direction.Decreasing,
+                    _ => direction
+                };
+                
+                direction ??= newDirection;
+                
+                if (direction != newDirection)
+                {
+                    safe = false;
+                    break;
+                }
+
+                if (Math.Abs(difference) is >= 1 and <= 3)
+                {
+                    continue;
+                }
+                
+                safe = false;
+            }
+            
+            safeReports += safe ? 1 : 0;
+        }
+
+        return safeReports.ToString();
+    }
+
+    public string Part2(string input)
+    {
+        return string.Empty;
+    }
+}

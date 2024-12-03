58e7f6903693aaa6fe00bd499e6431b2d761a7e8
Josh Perry <josh-perry@users.noreply.github.com>
2024-12-03 18:29:50 +0000

    Day 3 - part 1

---
+++ AoC24.Test/Day3.cs
@@ -0,0 +1,39 @@
+namespace AoC24.Test;
+
+public class Day3
+{
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part1()
+    {
+        // Arrange
+        var input = """
+                    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+                    """;
+        
+        var day = new Days.Day3();
+
+        // Act
+        var result = day.Part1(input);
+
+        // Assert
+        Assert.Equal("161", result);
+    }
+    
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part2()
+    {
+        // Arrange
+        var input = """
+                    
+                    """;
+
+        
+        var day = new Days.Day3();
+
+        // Act
+        var result = day.Part2(input);
+
+        // Assert
+        Assert.Equal("", result);
+    }
+}
---
+++ AoC24/Days/Day3.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC24.Days;
+
+public class Day3 : IDay
+{
+    public int Day => 3;
+
+    public string Part1(string input)
+    {
+        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+        
+        var matches = regex.Matches(input);
+        var total = 0;
+        
+        foreach (Match match in matches)
+        {
+            var firstNumber = int.Parse(match.Groups[1].Value);
+            var secondNumber = int.Parse(match.Groups[2].Value);
+
+            total += firstNumber * secondNumber;
+        }
+
+        return total.ToString();
+    }
+
+    public string Part2(string input)
+    {
+        return string.Empty;
+    }
+}
